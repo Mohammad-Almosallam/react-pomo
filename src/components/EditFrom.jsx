cfeeda27fc706a8668dfc5bfa2589500aff66c97
Mohammad Almosallam <moh.almusallam4@gmail.com>
2022-09-25 19:33:14 +0300

    fix: editFlied visibility

--- src/components/EditFrom.jsx
+++ src/components/EditFrom.jsx
@@ -14,12 +14,22 @@ export default function EidtInput(props) {
       }
     return 
   }
+
+  function handleSubmit(e){
+    if(numBreak === undefined || numTime === undefined){
+      alert('Please fill input with numbers')
+      return
+    }else{
+      props.onSubmit(e,numTime,numBreak)
+      return
+    }
+  }
   return (
     <div className={props.toggle ? "editInput editInput-active" : "editInput"}>
       <form>
         <input type={"number"} placeholder='Timer' value={numTime} min={"1"} onKeyUp={(e) => {handleZeroValue(e.target.value)}} onChange={(e)=>{setNumTime(e.target.value)}} className="input"/>
         <input type={"number"} placeholder='Break' value={numBreak} min={"1"} onKeyUp={(e) => {handleZeroValue(e.target.value)}} onChange={(e)=>{setNumBreak(e.target.value)}} className="input"/>
-        <button className='input submitBtn' type='submit' onClick={(e)=>{props.onSubmit(e,numTime,numBreak)}}>Submit</button>        
+        <button className='input submitBtn' type='submit' onClick={(e)=>{handleSubmit(e)}}>Submit</button>        
       </form>
     </div>
   )
